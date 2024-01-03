9ad8f461aa5ea926844f3b196b10ab28af162390
Bberkayyy <bberkayoguz@gmail.com>
2024-01-03 08:33:39 +0300

    admin panel theme changed

--- BusinessLayer/BusinessLayer/ValidationRules/PortfolioValidator.cs
+++ BusinessLayer/BusinessLayer/ValidationRules/PortfolioValidator.cs
@@ -8,10 +8,15 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.ValidationRules;
 
-public class PortfolioValidator:AbstractValidator<Portfolio>
+public class PortfolioValidator : AbstractValidator<Portfolio>
 {
-    public PortfolioValidator()
-    {
-        //RuleFor
-    }
+    //public PortfolioValidator()
+    //{
+    //    RuleFor(x => x.ProjectName).NotEmpty().WithMessage("Lütfen proje adı giriniz.");
+    //    RuleFor(x => x.ProjectName).MinimumLength(5).WithMessage("Proje adı en az 5 karakterden oluşmalıdır.");
+        
+    //    PortfolioValidator validations = new PortfolioValidator();
+    //    ValidationRules result = validations.Validate(x);
+
+    //}
 }
---
+++ PresentationLayer/PresentationLayer/Controllers/AdminDashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers;
+
+public class AdminDashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewBag.v1 = "Dashboard";
+        ViewBag.v2 = "İStatistikler";
+        return View();
+    }
+}
